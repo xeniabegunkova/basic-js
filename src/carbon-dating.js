de6a50e4a024633b741e2a30bf111eda4594054d
Ksenia Begunkova <kseniabegunkova@yandex.ru>
2023-04-12 12:38:15 +0300

    feat: done task about carbon (Wed, Apr 12, 2023, 12:38:02 AM)

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,6 +3,8 @@ const { NotImplementedError } = require('../extensions/index.js');
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
+const k = 0.693 / HALF_LIFE_PERIOD
+
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
@@ -17,11 +19,23 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  //throw new NotImplementedError('Not implemented');
+  if ((typeof sampleActivity !== 'string') || (isNaN(sampleActivity))) {
+    return false
+  }
+  else {
+    const year = - Math.log( sampleActivity / MODERN_ACTIVITY) / k;
+    //return Math.ceil(year)
+    return ((Math.ceil(year) !== Infinity) && (Math.ceil(year) > 0)) ? Math.ceil(year) : false; //согласно автотестам число не может быть бесконечным и отрицательным
+    //делаем конструкцию, где округленное вверх число будет отображаться, только при условии двух пунктов
+  }
 }
 
 module.exports = {
   dateSample
 };
+
+
+//https://gamer005.livejournal.com/54491.html?utm_source=3userpost - инфа о задаче
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/ceil - округление вверх
