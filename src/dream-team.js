4d17b17a8252df51f457fe26f7968811447ea7c2
Ksenia Begunkova <kseniabegunkova@yandex.ru>
2023-04-12 13:06:27 +0300

    feat: done task dream team (Wed, Apr 12, 2023, 13:06:02 AM)

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam( members ) {
+  //throw new NotImplementedError('Not implemented');
+
+  let arr = new Array();
+  if(!members || !Array.isArray(members)) {
+    return false
+  }
+
+  for (let name of members) {
+    if (typeof name === 'string') { //делаем пометку что нэйм это только строка
+      name = name.trim().toUpperCase(); //показываем что выводим эл. без пробелов большими буквами
+      arr.push(name[0]);
+    }
+  }
+  return arr.sort().join('');
 }
 
 module.exports = {
   createDreamTeam
 };
+
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
