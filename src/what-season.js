806de324fd53d54b96cb0a83e276755409669bf4
Ksenia Begunkova <kseniabegunkova@yandex.ru>
2023-04-17 12:07:05 +0300

    feat: done task seasons (Wed, Apr 17, 2023, 12:06:02 AM)

--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (typeof date === 'undefined') {
+    //If the date argument was not passed, the function must return the string 'Unable to determine the time of year!'.
+    return 'Unable to determine the time of year!'
+  }
+  if (Object.prototype.toString.call(date) !== "[object Date]") { //Time of the year must be string.
+    throw new Error("Invalid date!");
+  }
+  if (date instanceof Date) {
+    try { date.getUTCFullYear(); }
+    catch {
+      throw new Error("Invalid date!");
+    }
+    const months = date.getMonth(); //возвращает месяц указанной даты по местному времени
+
+    if (months === 2 || months === 3 || months === 4) {
+      return 'spring'
+    }
+    if (months === 5 || months === 6 || months === 7) {
+      return 'summer'
+    }
+    if (months === 8 || months === 9 || months === 10) {
+      return 'autumn'
+    }
+    else {
+      return 'winter'
+    }
+  }
+  else {
+    return new Error("Invalid date!");
+  }
 }
 
 module.exports = {
   getSeason
 };
+
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Date/getMonth
+//https://learn.javascript.ru/exception
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/instanceof
+//https://stackoverflow.com/questions/643782/how-to-check-whether-an-object-is-a-date
