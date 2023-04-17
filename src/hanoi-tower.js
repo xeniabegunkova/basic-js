6e5c1f5385b35b1872c9ebb949ff63a03077fdde
Ksenia Begunkova <kseniabegunkova@yandex.ru>
2023-04-17 12:37:53 +0300

    feat: done task hanoi tower (Wed, Apr 17, 2023, 12:37:02 AM)

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -5,7 +5,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * to solve puzzle
  * 
  * @param {Number} disks number of disks
- * @param {Number} turnsSpeed speed (in turns/hour)
+ * @param {Number} turnsSpeed speed (in turns/hour) поэтому делимм на 3600
  * @return {Object} object with props turns (number of turns)
  * and seconds (time in seconds)
  *
@@ -14,11 +14,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+  const moveM = { //Функция calculateHanoi возвращает объект с 2 свойствами
+    turns: 0, //повороты (минимальное количество поворотов для решения головоломки)
+    seconds: 0 //секунды (минимальное количество секунд для решения головоломки при заданной скорости поворота)
+  }
+  moveM.turns = 2 ** disksNumber - 1; //Mn = 2**n - 1
+  moveM.seconds = Math.floor(moveM.turns / (turnsSpeed / 3600)); //секунды должны быть целым числом, полученным из округленного в меньшую сторону результата расчета (этаж))
+  return moveM;
 }
+
 
 module.exports = {
   calculateHanoi
 };
+
+
+//https://synset.com/ai/ru/search/Towers_of_Hanoi.html
