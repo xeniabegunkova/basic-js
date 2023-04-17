881e99808ad4ac1251e1553641c718fd9f1fa6fe
Ksenia Begunkova <kseniabegunkova@yandex.ru>
2023-04-17 15:09:50 +0300

    feat: done task matrix (Wed, Apr 17, 2023, 14:37:02 AM)

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,11 +16,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let total = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        for (let k = i + 1; k < matrix.length; k++) {
+          matrix[k][j] = 0;
+        }
+      }
+    }
+  }
+
+  for (let x = 0; x < matrix.length; x++) {
+    for (let y = 0; y < matrix[x].length; y++) {
+      total += matrix[x][y]
+    }
+  }
+
+  return total;
 }
 
 module.exports = {
   getMatrixElementsSum
 };
+
+//https://medium.com/@aaronmassey45/180-days-of-code-day-137-e723e8e3dcaf
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,11 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  /*for (j = 0; j < arr.length - 1; j++) {
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i]>arr[i+1]) {
+        let temp = arr[i]
+        arr[i] = arr[i+1]
+        arr[i+1] = temp;
+      }
+    }
+  }*/
+
+
 }
 
 module.exports = {
   sortByHeight
 };
+
+//https://www.techiedelight.com/ru/sort-array-of-objects-javascript/
